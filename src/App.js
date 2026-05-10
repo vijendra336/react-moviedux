601fbc7f4663a9364b97d4e323063270f766607a
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2026-05-10 19:55:44 +0530

    4. Added Header and Footer Content in Root Component App.js.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1> Welcome to MovieDux App</h1>
       </header>
+      <footer className="footer">
+        <p className="footer">
+          Footer Content :Copyright © 2026 MovieDux. All rights reserved.
+        </p>
+      </footer>
     </div>
   );
 }
