b36e28a7ec1cb49de52f8a84cfae04a5171aec41
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2026-05-27 21:50:14 +0530

    10 need to set default image if image not found onError.

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,9 +2,17 @@ import React from "react";
 import "../styles.css";
 
 const MovieCard = ({ movie }) => {
+  const handleError = (e) => {
+    e.target.src = "images/default.jpg"; // Fallback image
+  };
+
   return (
     <div className="movie-card">
-      <img src={`images/${movie.image}`} alt={movie.title} />
+      <img
+        src={`images/${movie.image}`}
+        alt={movie.title}
+        onError={handleError}
+      />
       <div className="movie-card-info">
         <h3 className="movie-card-title">{movie.title}</h3>
         <p className="movie-card-genre">{movie.genre}</p>
