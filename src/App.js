ec0de966f770ebc9244ea62a43c0c9b2add8483e
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2026-05-28 22:41:39 +0530

    14.2 Created Watchlist component and routes Home and Watchlist.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,34 @@ import "./styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MovieGrid from "./components/MovieGrid";
+import Watchlist from "./components/Watchlist";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header> </Header>
+
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MovieGrid />} />
+            <Route path="/watchlist" element={<Watchlist />} />
+          </Routes>
+        </Router>
       </div>
-      <MovieGrid> </MovieGrid>
+
       <Footer> </Footer>
     </div>
   );
---
+++ src/components/Watchlist.js
@@ -0,0 +1,10 @@
+import react from "react";
+import "../styles.css";
+
+export default function Watchlist() {
+  return (
+    <div>
+      <h1 className="title">Your watchlist is currently empty.</h1>
+    </div>
+  );
+}
